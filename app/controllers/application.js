02be94978a5ab21ff7326e838e924a4103edde07
monisha.sa <monisha.sa@zohocorp.com>
2020-08-24 18:16:11 +0530

    ember get location success

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,25 +1,37 @@
 import Controller from '@ember/controller';
-
 export default Controller.extend({
   init() {
+    this._super(...arguments);
     this.handlePermission();
   },
 
   handlePermission() {
+    let self = this;
+    let geoSettings = {
+      enableHighAccuracy: false,
+      maximumAge        : 30000,
+      timeout           : 20000
+    };
     navigator.permissions.query({name:'geolocation'}).then(function(result) {
       if (result.state == 'granted') {
-        //this.report(result.state);
-        //geoBtn.style.display = 'none';
+        self.report(result.state);
+        self.set('styleProperty', 'display: none;');
+      //  geoBtn.style.display = 'none';
       } else if (result.state == 'prompt') {
-        //this.report(result.state);
-        //geoBtn.style.display = 'none';
-        navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
+        self.report(result.state);
+        self.set('styleProperty', 'display: none;');
+      //  geoBtn.style.display = 'none';
+      if (!"geolocation" in navigator) {
+        alert("No geolocation available!");
+      }
+        navigator.geolocation.getCurrentPosition(self.revealPosition,self.positionDenied,geoSettings);
       } else if (result.state == 'denied') {
-        //report(result.state);
+        self.report(result.state);
+        self.set('styleProperty', 'display: inline;');
         //geoBtn.style.display = 'inline';
       }
       result.onchange = function() {
-        //this.report(result.state);
+        self.report(result.state);
       }
     });
   },
@@ -29,7 +41,22 @@ export default Controller.extend({
   },
 
   revealPosition(position) {
-    console.log('reveal');
+    var markerTitle = "You are here";
+
+    var latlng = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+    var myOptions = {
+      zoom: 16,
+      center: latlng,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    var map = new google.maps.Map(mapCanvas, myOptions);
+
+    var marker = new google.maps.Marker({
+      position: latlng,
+      map: map,
+      title: markerTitle
+    });
   },
 
   positionDenied() {
