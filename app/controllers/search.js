cbca21c6bac69d6c52c53ccae8f582dec8637d98
surya.e <surya.e@zohocorp.com>
2020-08-25 19:54:10 +0530

    fix: assertion error

--- app/controllers/search.js
+++ app/controllers/search.js
@@ -1,22 +1,22 @@
 import Controller from '@ember/controller';
 import fetch from 'fetch';
+import { action } from '@ember/object';
 import { OPEN_WEATHER_CONSTANTS } from '../constants/OPEN_WEATHER_CONSTANTS';
-
-// TODO: Need to fix Assertion error on calling this.set
 
 export default Controller.extend({
   init() {
     this._super(...arguments);
     this.set('status', 'empty');
   },
+  @action
   async search(event) {
+    if (event.currentTarget.value.length === 0) {
+      this.set('status', 'empty');
+      return;
+    }
     try {
       var value = event.currentTarget.value;
-      // Checking for empty string
-      if (value.length === 0) {
-        this.set('status', 'empty');
-        return;
-      }
+      console.log(event.currentTarget.value);
       this.set('status', 'pending');
       var response = await fetch(`${OPEN_WEATHER_CONSTANTS.BASE_URL}?q=${value}&appid=${OPEN_WEATHER_CONSTANTS.API_KEY}`);
       var json = await response.json();
@@ -27,4 +27,10 @@ export default Controller.extend({
       this.set('status', 'resolved');
     }
   },
+  @action
+  handleInput(event) {
+    if (event.currentTarget.value === "") {
+      this.set('status', 'empty');
+    }
+  }
 });
