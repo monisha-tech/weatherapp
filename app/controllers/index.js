cc8d594000b2e045fb197b459d4f5f3b5528c2ad
SuryaElavazhagan <surya.elash98@protonmail.com>
2020-08-28 17:44:17 +0530

    Fix: Service worker registration (#7)
    * chore: removed unused dependency
    
    * fix: added correct url path
    
    * chore: removed console logs
    
    Co-authored-by: surya.e <surya.e@zohocorp.com>

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -17,30 +17,17 @@ export default Controller.extend({
     };
     navigator.permissions.query({name:'geolocation'}).then(function(result) {
       if (result.state == 'granted') {
-        self.report(result.state);
         navigator.geolocation.getCurrentPosition(self.revealPosition.bind(self),self.positionDenied,geoSettings);
-      //  geoBtn.style.display = 'none';
       } else if (result.state == 'prompt') {
-        self.report(result.state);
         self.set('styleProperty', 'display: none;');
-      //  geoBtn.style.display = 'none';
       if (!("geolocation" in navigator)) {
         alert("No geolocation available!");
       }
         navigator.geolocation.getCurrentPosition(self.revealPosition.bind(self),self.positionDenied,geoSettings);
       } else if (result.state == 'denied') {
-        self.report(result.state);
         self.set('styleProperty', 'display: inline;');
-        //geoBtn.style.display = 'inline';
-      }
-      result.onchange = function() {
-        self.report(result.state);
       }
     });
-  },
-
-  report(state) {
-    console.log('Permission ' + state);
   },
 
   async revealPosition(position) {
@@ -49,11 +36,10 @@ export default Controller.extend({
     var response = await fetch(`${OPEN_WEATHER_CONSTANTS.BASE_URL}?lat=${latitude}&lon=${longitude}&appid=${OPEN_WEATHER_CONSTANTS.API_KEY}`);
     var json = await response.json();
     this.set('data', json);
-    console.log(json);
   },
 
   positionDenied() {
-    console.log('denied');
+    // Need to handle permission denied
   },
 
   getdata(json) {
--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'weatherapp',
     environment,
-    rootURL: '/',
+    rootURL: '/weatherapp/',
     locationType: 'hash',
     EmberENV: {
       FEATURES: {
